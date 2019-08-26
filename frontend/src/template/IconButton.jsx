395ad4059a8a62c9547203c42cc1abc7b53aeb4d
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-08-26 17:13:40 -0300

    Refatorando Comp Button com If2

--- frontend/src/template/IconButton.jsx
+++ frontend/src/template/IconButton.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import If from './If'
 //Componente botão
 
-export default props => {
-    if(props.hide) {
-        return null
-    } else {
-        return (
+export default props => (
+    <If test={!props.hide}>
             <Button className='btn'
                 variant={props.bstyle}
                 onClick={props.onClick}>
                     <i className={'fa fa-' + props.icon}></i>
             </Button>
+    </If>
         )
-    }
-}

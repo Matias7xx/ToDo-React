46d9e8541326feb9dcdedd61d7f937b5798d80ac
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-08-31 12:27:44 -0300

    Configurando o Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 //import App from './App';
+import { createStore } from 'redux'; //REDUX
+import { Provider } from 'react-redux' //Integração entre React e Redux
+
 import App from  './main/App'
+import reducers from './main/reducers' //Redux Reducers
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

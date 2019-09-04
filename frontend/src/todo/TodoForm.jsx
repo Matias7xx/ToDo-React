387e749eb95fafdb20d743299c54e1a9610c4c4a
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-09-04 12:11:02 -0300

    Action Creator de Pesquisa e Refac TodoForm para componente de cclasse

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
@@ -7,47 +7,52 @@ import IconButton from '../template/IconButton'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
+class TodoForm extends Component {
 
-    const keyHandler = (e) => { //Teclas de atalho
+    componentDidMount() { //Vai carregar a lista de items imediatamente após o componente ser renderizado
+        this.props.search()
+    }
+
+    keyHandler = (e) => { //Teclas de atalho
         if(e.key === 'Enter') {
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
         } else if (e.key === 'Escape') {
-            props.handleClear()
+            this.props.handleClear()
         }
     }
 
-    return (
-        <Form className='todoForm'>
+    render() {
+        return (
+            <Form className='todoForm'>
             <Row>
                 <Col xs={12} sm={9} md={10}>
                     <Form.Group controlId="description">
                         <Form.Control type="text" placeholder="Adicione uma tarefa"
-                        onChange={props.changeDescription}
-                        onKeyUp={keyHandler}
-                        value={props.description} />
+                        onChange={this.props.changeDescription}
+                        onKeyUp={this.keyHandler}
+                        value={this.props.description} />
                     </Form.Group>
                 </Col>
     
                 <Col xs={12} sm={3} md={2}>
                     <IconButton bstyle='primary' icon='plus'
-                        onClick={props.handleAdd}></IconButton>
+                        onClick={this.props.handleAdd}></IconButton>
                     <IconButton bstyle='info' icon='search'
-                        onClick={props.handleSearch}></IconButton>
+                        onClick={this.props.handleSearch}></IconButton>
                     <IconButton bstyle='secondary' icon='close'
-                        onClick={props.handleClear}></IconButton>
+                        onClick={this.props.handleClear}></IconButton>
                 </Col>
             </Row>
         </Form>
-    )
-    
+        )
+    }
 }
 
 const mapStateToProps = state => ({
     description: state.todo.description
 })
 const mapDispatchToProps = dispatch => //Dispara a ação e passa ela para os reducers
-    bindActionCreators({ changeDescription }, dispatch);
+    bindActionCreators({ changeDescription, search }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,5 +1,18 @@
 //Actions do Todo
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
+
 export const changeDescription = (event) => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+//Action Creator que vai buscar os serviços do BackEnd
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
